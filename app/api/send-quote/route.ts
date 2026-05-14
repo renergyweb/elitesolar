65fbc2483c2248cf363afb3f08a6cbe9fd243db7
VictorPunk2077 <victorpunk2077@gmail.com>
2026-05-13 22:01:54 -0600

    Update Resend API key

--- app/api/send-quote/route.ts
+++ app/api/send-quote/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend('re_NkdFkmiy_JTgfybtMC3bxHokXdS9sB5XA');
+const resend = new Resend(process.env.RESEND_API_KEY || 're_c4dX7crJ_LhBKSqDHWURUQj19aUHhdwJ9');
 
 export async function POST(request: Request) {
   try {
