aebdca806140fb0d0cdfc1d7044a3239e4f9f4b8
renergyweb <renergy.web@gmail.com>
2026-07-13 17:35:05 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: 'Cotizador Solar IA',
+  title: 'Renergy México',
   description: 'Cotizador de paneles solares en México usando Gemini AI',
   icons: {
     icon: 'https://i.postimg.cc/bwFyc87p/renergy-logotipo.webp',
